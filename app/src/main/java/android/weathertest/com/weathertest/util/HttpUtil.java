aae392dee5300ff1429f3737fbcccc9dcb14ba7c
gaoyaning <gaoyaning@sagereal.com>
2018-04-13 16:19:59 +0800

    省市县三级目录的遍历
    Change-Id: Iaa90baeddfbc02264b0d5b597b8a40032618d21c

---
+++ app/src/main/java/android/weathertest/com/weathertest/util/HttpUtil.java
@@ -0,0 +1,21 @@
+package android.weathertest.com.weathertest.util;
+
+import android.view.ActionMode;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by gyn on 18-4-12.
+ */
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback){
+        //1.初始化一个OkHttpClient
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+
+    }
+}
---
+++ app/src/main/java/android/weathertest/com/weathertest/util/Utility.java
@@ -0,0 +1,79 @@
+package android.weathertest.com.weathertest.util;
+
+import android.text.TextUtils;
+import android.weathertest.com.weathertest.db.City;
+import android.weathertest.com.weathertest.db.County;
+import android.weathertest.com.weathertest.db.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by gyn on 18-4-12.
+ */
+
+public class Utility {
+    public static boolean handleProvinceResponse(String response) {
+        if (!TextUtils.isEmpty(response)) {
+            try {
+                JSONArray allProvince = new JSONArray(response);
+                for (int i = 0; i < allProvince.length(); i++) {
+                    JSONObject provinceObject = allProvince.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return false;
+    }
+
+    public static boolean handleCityResponse(String response ,int provinceId) {
+        if (!TextUtils.isEmpty(response)) {
+            try {
+                JSONArray allCity = new JSONArray(response);
+                for (int i = 0; i < allCity.length(); i++) {
+                    JSONObject cityObject = allCity.getJSONObject(i);
+                    City city = new City();
+                    city.setCityName(cityObject.getString("name"));
+                    city.setCityCde(cityObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    //DateSupport中的方法
+                    city.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return false;
+    }
+
+    public static boolean handleCountyResponse(String response ,int cityId) {
+        if (!TextUtils.isEmpty(response)) {
+            try {
+                JSONArray allCounty = new JSONArray(response);
+                for (int i = 0; i < allCounty.length(); i++) {
+                    JSONObject countyObject = allCounty.getJSONObject(i);
+                    County county = new County();
+                    county.setCountyName(countyObject.getString("name"));
+                    county.setWeatherId(countyObject.getString("weather_id"));
+                    county.setCityId(cityId);
+                    county.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return false;
+    }
+}
